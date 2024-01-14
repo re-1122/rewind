0d51238b25bb1c2adcb874244e86c245d8d7e393
Komichi <k0michi@koyomi.co>
2024-01-14 19:06:02 +0900

    Prevent end spikes from overwriting blocks when generated

--- src/main/java/com/koyomiji/rewind/mixin/world/gen/feature/MixinWorldGenSpikes.java
+++ src/main/java/com/koyomiji/rewind/mixin/world/gen/feature/MixinWorldGenSpikes.java
@@ -2,14 +2,20 @@ package com.koyomiji.rewind.mixin.world.gen.feature;
 
 import com.koyomiji.rewind.config.ReWindConfig;
 import com.koyomiji.rewind.world.gen.feature.OldEndSpike;
+import net.minecraft.block.state.IBlockState;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
 import net.minecraft.world.gen.feature.WorldGenSpikes;
+import net.minecraft.world.gen.feature.WorldGenerator;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Constant;
 import org.spongepowered.asm.mixin.injection.ModifyConstant;
+import org.spongepowered.asm.mixin.injection.Redirect;
 
 @Mixin(WorldGenSpikes.class)
-public class MixinWorldGenSpikes {
+public abstract class MixinWorldGenSpikes extends WorldGenerator {
   @Shadow private WorldGenSpikes.EndSpike spike;
 
   @ModifyConstant(method = "generate", constant = @Constant(intValue = 0))
@@ -20,4 +26,13 @@ public class MixinWorldGenSpikes {
 
     return original;
   }
+
+  @Redirect(method = "generate", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/gen/feature/WorldGenSpikes;setBlockAndNotifyAdequately(Lnet/minecraft/world/World;Lnet/minecraft/util/math/BlockPos;Lnet/minecraft/block/state/IBlockState;)V", ordinal = 1))
+    private void mixin2(WorldGenSpikes instance, World world, BlockPos blockPos, IBlockState iBlockState) {
+        if (ReWindConfig.classicEndSpikes) {
+          return;
+        }
+
+        setBlockAndNotifyAdequately(world, blockPos, iBlockState);
+    }
 }
