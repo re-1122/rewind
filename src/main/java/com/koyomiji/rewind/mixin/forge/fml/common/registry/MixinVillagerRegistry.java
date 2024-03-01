efc03d14c7cc342909c5b39909ddd333da630306
Komichi <k0michi@koyomi.co>
2024-03-01 10:35:53 +0900

    Disable MixinVillagerRegistry remapping

--- src/main/java/com/koyomiji/rewind/mixin/forge/fml/common/registry/MixinVillagerRegistry.java
+++ src/main/java/com/koyomiji/rewind/mixin/forge/fml/common/registry/MixinVillagerRegistry.java
@@ -10,7 +10,7 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Redirect;
 
-@Mixin(value = VillagerRegistry.class, remap = false)
+@Mixin(value = VillagerRegistry.class)
 public class MixinVillagerRegistry {
   @Redirect(
       method =
